06f095d27d0d6366bd74c7793224e0a1f2d37b1c
AnAppleCore <994164148@qq.com>
2020-12-24 22:13:29 +0800

    update

--- lab5.sh
+++ lab5.sh
@@ -47,7 +47,6 @@ ip netns exec ns02 ifconfig veth23 up
 
 ip netns exec ns02 sysctl net.ipv4.ip_forward=1
 ip netns exec ns02 iptables -t nat -A POSTROUTING -o veth23 -j MASQUERADE
-ip route add 10.0.0.6 via 10.0.0.2
 ip route add 10.0.0.6 via 10.0.0.4
 
 # ns03 
