4f96f2660e01b155e944392c3ed30916373fbf7f
AnAppleCore <994164148@qq.com>
2020-12-25 18:26:43 +0800

    update

--- lab5.sh
+++ lab5.sh
@@ -3,18 +3,21 @@
 ip netns del ns01
 ip netns del ns02
 ip netns del ns03
+ip netns del ns04
 
 ################################################################
 ###################### create namespaces #######################
 ip netns add ns01
 ip netns add ns02
 ip netns add ns03
+ip netns add ns04
 
 ################################################################
 #################### create pairs of veths ####################
 ip link add veth01 type veth peer name veth10
 ip link add veth12 type veth peer name veth21
 ip link add veth23 type veth peer name veth32
+ip link add veth34 type veth peer name veth43
 
 ################################################################
 ###### assign veths to namespaces, set ip addresses, ... #######
@@ -25,6 +28,7 @@ ifconfig veth01 up
 
 ip route add 10.0.0.4 via 10.0.0.2
 ip route add 10.0.0.6 via 10.0.0.2
+ip route add 10.0.0.8 via 10.0.0.2
 
 # ns01
 ip link set veth10 netns ns01
@@ -39,6 +43,7 @@ ip netns exec ns01 sysctl net.ipv4.ip_forward=1
 ip netns exec ns01 iptables -t nat -A POSTROUTING -o veth12 -j MASQUERADE
 
 ip netns exec ns01 ip route add 10.0.0.6 via 10.0.0.4
+ip netns exec ns01 ip route add 10.0.0.8 via 10.0.0.4
 
 # ns02
 ip link set veth21 netns ns02
@@ -52,11 +57,24 @@ ip netns exec ns02 ifconfig veth23 up
 ip netns exec ns02 sysctl net.ipv4.ip_forward=1
 ip netns exec ns02 iptables -t nat -A POSTROUTING -o veth23 -j MASQUERADE
 
+ip netns exec ns02 ip route add 10.0.0.8 via 10.0.0.6
+
 # ns03 
 ip link set veth32 netns ns03
 
 ip netns exec ns03 ip addr add 10.0.0.6 peer 10.0.0.5 dev veth32
 ip netns exec ns03 ifconfig veth32 up
+ip netns exec ns03 ip addr add 10.0.0.7 peer 10.0.0.8 dev veth34
+ip netns exec ns03 ifconfig veth34 up
+
+ip netns exec ns03 sysctl net.ipv4.ip_forward=1
+ip netns exec ns03 iptables -t nat -A POSTROUTING -o veth34 -j MASQUERADE
+
+# ns04
+ip link set veth43 netns ns04
+
+ip netns exec ns04 ip addr add 10.0.0.8 peer 10.0.0.7 dev veth43
+ip netns exec ns04 ifconfig veth43 up
 
 ################################################################
 ####################### hosts settings #########################
@@ -64,4 +82,4 @@ cp /home/aac/I2CN_lab/lab5.hosts /etc/hosts
 
 ################################################################
 ############################ test ##############################
-traceroute 10.0.0.6
+traceroute 10.0.0.8
